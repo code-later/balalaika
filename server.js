aeae37e956b3970cd9fa462065edbafeb1e4067b
Sebastian Cohnen <sebastian.cohnen@gmx.net>
2011-11-26 11:29:34 +0100

    added READ /issues to fetch data from CouchDB

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express')
   , app = express.createServer()
   , io = require('socket.io').listen(app)
+  , http = require("http")
 
 app.configure(function() {
   app.use(express.bodyParser())
@@ -11,20 +12,31 @@ app.configure(function() {
 var id = 0
 
 var issues = io.of("/issues").on('connection', function (socket) {
-  // setInterval(function() {
-  //   socket.emit("new", {
-  //     id: id++, subject: "New issue " + id
-  //   })
-  // }, 1500)
+  socket.on("read", function (request) {
+    http.request({
+      host: "localhost",
+      port: 5984,
+      path: "/hotboy_inc_development/_all_docs?include_docs=true",
+      method: "GET"
+    }, function (res) {
+      var body = ""
+      res.on("data", function (chunk) { body += chunk })
+      res.on("end", function() {
+        var parsed_response = JSON.parse(body)
 
-  socket.on('get', function (data) {
-    
-    var response = {
-      foo: "bar",
-      issue_number: ++id
-    }
-    
-    socket.emit("response", {header: {response_id: data.header.response_id}, payload: response})
+        var payload = parsed_response.rows.map(function(row) {
+          var doc = row.doc
+          doc.id = doc._id
+          doc.rev = doc._rev
+          return row.doc
+        })
+
+        socket.emit("response", {
+          header: {response_id: request.header.response_id},
+          payload: payload
+        })
+      })
+    }).end()
   })
 })
 
